56fc6ebae781b917d4557ad473c20afcf8e5c68c
TGit <tilo_weber_berlin@web.de>
2013-01-13 10:52:34 +0100

    add new package esa.titan.taskplanner add User and Login class for taskplanner

---
+++ Titan/src/main/java/esa/titan/taskplanner/Login.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esa.titan.taskplanner;
+
+/**
+ *
+ * @author tiloW7-2012
+ */
+public class Login {
+    
+}
