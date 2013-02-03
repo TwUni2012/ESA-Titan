ae5a6c02a353a0796c8d05515560efbe2e2ec925
tilo <tilo_weber_berlin@web.de>
2013-02-03 03:19:22 +0100

    add more rest-link categories speed up article loading

---
+++ Titan/src/main/java/esa/titan/restful/newsticker/Loader.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esa.titan.restful.newsticker;
+
+import java.util.ArrayList;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author tilo
+ */
+public class Loader {
+    
+    private LoadArticles loadArticles;
+
+    public Loader() {
+    }
+    
+    public Loader(String url) {
+        loadArticles = new LoadArticles(url);
+    }
+    
+        public void loadFirstElement() {
+
+//        loadArticles = new LoadArticles();
+        Article firstArticle = loadArticles.loadFirstArticle();
+        ArticleManager.clear();
+        ArticleManager.addArticle(firstArticle);
+    }
+
+    @Asynchronous
+    public void loadAllOtherArticleElements() {
+        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### loadAllOtherArticleElements: ");
+        ArrayList<Article> nextArticles = loadArticles.nextArticles();
+        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### nextArticles-size: " + nextArticles.size());
+        ArticleManager.addArticle(nextArticles);
+    }
+    
+}
--- Titan/src/main/java/esa/titan/restful/newsticker/RestfulLink.java
+++ Titan/src/main/java/esa/titan/restful/newsticker/RestfulLink.java
@@ -5,20 +5,17 @@
 package esa.titan.restful.newsticker;
 
 import java.awt.event.ActionEvent;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ejb.EJB;
-import javax.ejb.Stateful;
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateless;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
-import javax.inject.Named;
 
 /**
  *
@@ -28,10 +25,6 @@ import javax.inject.Named;
 @SessionScoped
 public class RestfulLink implements Serializable{
 
-    // TODO Refactoring, Fehlerbehandlung, wenn Article nicht verfuegbar, bessere Objektverwaltung, Laden der Artikel in Asynchrone Methode auslagern
-    
-//    @EJB
-//    private ArticleManager articleManager;
     private String domain = "http://content.guardianapis.com/";
     private String category = "world";
     private String format = "?format=xml";
@@ -41,22 +34,25 @@ public class RestfulLink implements Serializable{
     private String[] countries = {"Hallo", "Welt", "Ich", "Hammer", "Blub"};
     private List<SelectItem> categories = new LinkedList<SelectItem>();
     private String selected = "world";
+    private LoadArticles loadArticles;
+    private Loader l;
+    private boolean loaded = false;
+    
     public RestfulLink() {
-        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### RestfulLink Objekt wurde erstellt");
-//        IncrementCounterThread i = new IncrementCounterThread(this);
-        
+        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### RestfulLink Objekt wurde erstellt");     
                     
         categories.add((new SelectItem("world", "world")));
         categories.add(new SelectItem("sport", "sport"));
-//        i.run();
-        loadURK();
+        categories.add(new SelectItem("football", "football"));
+        categories.add(new SelectItem("stage", "stage"));
+        categories.add(new SelectItem("music", "music"));
+        categories.add(new SelectItem("travel", "travel"));
+        categories.add(new SelectItem("film", "film"));
+        categories.add(new SelectItem("politics", "politics"));
+        
+        //loadFirstElement
+        loadURL();
     }
-
-    //        categories.add(new SelectItem("sport", "sport"));
-//        categories.add((new SelectItem("world", "world")));
-
-//        IncrementCounterThread i = new IncrementCounterThread(this);
-//        i.run();
 
     public String getSelected() {
         return selected;
@@ -73,7 +69,6 @@ public class RestfulLink implements Serializable{
     public void setCategories(List<SelectItem> categories) {
         this.categories = categories;
     }
-    
     
     public String getCategory() {
         return category;
@@ -109,7 +104,6 @@ public class RestfulLink implements Serializable{
         }
         String result = countries[counter];
         counter++;
-//        return Long.toString(System.currentTimeMillis());
         return result;
     }
 
@@ -117,27 +111,51 @@ public class RestfulLink implements Serializable{
         this.time = time;
     }
 
-    public String loadURK() {
+    public void loadURL() {
         String url = getUrl();
         Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "#### loadURK: " + url);
-        LoadArticles la = new LoadArticles();
-        ArrayList<Article> articles = la.parseHTML(url);
-        ArticleManager.setArticles(articles);
-
-        return "Load";
+//        loadArticles = new LoadArticles(url);
+//        ArrayList<Article> articles = loadArticles.parseHTML(url);
+//        ArticleManager.setArticles(articles);
+        l = new Loader(url);
+        l.loadFirstElement();
+        loaded = true;
+        
+//        return "Load";
     }
 
-//    public int getCounter() {
-//        return counter;
-//    }
-//
-//    public void setCounter(int counter) {
-//        this.counter = counter;
-//    }
+    public void loadAllOtherArticleElements() {
+        if((l != null) && (loaded == true)) {
+            l.loadAllOtherArticleElements();
+            loaded = false;
+        }
+    }
+    
+    /*
+    private void loadFirstElement() {
+
+//        loadArticles = new LoadArticles();
+        Article firstArticle = loadArticles.loadFirstArticle();
+        ArticleManager.clear();
+        ArticleManager.addArticle(firstArticle);
+    }
+
+    @Asynchronous
+    private void loadAllOtherArticleElements() {
+
+        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### loadAllOtherArticleElements: ");
+                try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(RestfulLink.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### loadAllOtherArticleElements after 10 sec sleep");
+        ArrayList<Article> nextArticles = loadArticles.nextArticles();
+        Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### nextArticles-size: " + nextArticles.size());
+        ArticleManager.addArticle(nextArticles);
+    }
+ */   
     public void parse(ActionEvent e) {
         Logger.getLogger(RestfulLink.class.getName()).log(Level.INFO, "### Link soll geparst werden");
-//        XMLParser xmlP = new XMLParser(this.getUrl());
-//        xmlP.parse();
-//         xMLParser.parse('http://content.guardianapis.com')
     }
 }
