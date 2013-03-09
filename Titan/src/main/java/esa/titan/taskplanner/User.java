048d1e7126804bddfc58ded08ac2e98c21d1a89f
tilo <tilo@tiloUbuntu64bitDesktop2013>
2013-03-09 11:40:53 +0100

    taskplanner: add testerUser "tester, tester" taskplanner: add calendar

--- Titan/src/main/java/esa/titan/taskplanner/User.java
+++ Titan/src/main/java/esa/titan/taskplanner/User.java
@@ -4,17 +4,24 @@
  */
 package esa.titan.taskplanner;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author tiloW7-2012
  */
+@ManagedBean
+//@RequestScoped
 public class User {
-    private String name;
-    private String password;
+    private String name = "";
+    private String password = "";
     
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
+    public User() {
+        Logger.getLogger(User.class.getName()).log(Level.INFO, "new User");
     }
 
     public String getName() {
@@ -31,5 +38,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public String login() {
+        Logger.getLogger(User.class.getName()).log(Level.INFO, "Name: " + this.name);
+        Logger.getLogger(User.class.getName()).log(Level.INFO, "Password: " + this.password);
+        // database access to validate the users
+        if(name.equalsIgnoreCase("tester") && password.equalsIgnoreCase("tester")) {
+            return "taskplannerLogin";
+        } else {
+            return "";
+        }
     }
 }
